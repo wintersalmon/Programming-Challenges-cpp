4a8f037305eedb4a81346589c3b16c68133ae534
wintersalmon <salmonjoon@gmail.com>
2016-02-16 15:43:44 +0900

    Initial Commit

---
+++ ProgrammingChallenges/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ProgrammingChallenges
+//
+//  Created by Salmon on 2016. 2. 16..
+//  Copyright © 2016년 Salmonjoon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
